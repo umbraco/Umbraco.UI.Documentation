991dc5889734ca49ead97db878e7c71f79bc39b1
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-03-04 12:33:08 +0100

    Add container layout element

---
+++ src/layout/container.element.ts
@@ -0,0 +1,28 @@
+import { css, html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+@customElement('x-container')
+export class Container extends LitElement {
+    static styles = css`
+        :host {
+            text-align: center;
+        }
+
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+    `;
+
+    @property()
+    headline?: string;
+
+    render() {
+        return html`
+            <article class="container">
+                ${this.headline ? html`<header class="header"><h1>${this.headline}</h1></header>` : ''}
+                <slot></slot>
+            </article>
+        `;
+    }
+}
