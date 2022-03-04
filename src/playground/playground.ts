4f0a6e91f1fd5ca8f2a99f2edd0ef0c12f5049b8
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-03-04 10:19:26 +0100

    Add some container styling for Playground

--- src/playground/playground.ts
+++ src/playground/playground.ts
@@ -1,13 +1,57 @@
-import { html, LitElement } from 'lit';
+import 'playground-elements/playground-ide.js';
+
+import { css, html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
 
 @customElement('x-playground')
 export class Playground extends LitElement {
+    static styles = css`
+        #container {
+            display: grid;
+            grid-template-columns: 200px 1fr;
+        }
+
+        #header {
+            background: gray;
+            text-align: center;
+        }
+
+        #topnav-content {
+            background: blue;
+        }
+
+        #sidenav {
+            display: flex;
+            flex-wrap: wrap;
+            flex-direction: column
+        }
+
+        #sidenav a {
+            padding: 0.5rem;
+            margin: 0.5rem;
+        }
+    `;
+
     render() {
         return html`
-        <h1>Basic examples</h1>
-    `;
+            <div id="topnav">
+            </div>
+            <div id="container">
+                <header id="header">
+                    <h2>Playground</h2>
+                </header>
+                <div id="topnav-content"></div>
+                <div id="sidenav">
+                    <a class="active" href="/playground">Playground</a>
+                    <a href="/playground/basics">Basics</a>
+                </div>
+
+                <div id="content">
+                    <slot></slot>
+                </div>
+            </div>
+        `;
     }
 }
 
