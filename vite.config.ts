bda9e4dd26038c1e5b3d4965b83301cacc5a5a68
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-03-04 13:40:53 +0100

    Add rollup-plugin-cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+import cleanup from 'rollup-plugin-cleanup';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
+      plugins: [cleanup({ comments: "none" })],
     }
   }
 })
